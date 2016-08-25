297596a9012f87e57e9b0c171d795bfaac601da4
jense-arntz <jense_arntz@mail.com>
2016-08-25 23:50:10 +0200

    fix

--- Code/start_node.sh
+++ Code/start_node.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
-### BEGIN INIT INFO
-# Provides:          node_server
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Example initscript
-# Description:       This file should be used to construct scripts to be
-#                    placed in /etc/init.d.
-### END INIT INFO
-
 export PATH=$PATH:/usr/local/bin
 export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
-#export SERVER_PORT=8081
-#export SERVER_IFACE='192.168.1.111'
+
 
 case "$1" in
   start)
-  exec forever --sourceDir=/home/RFID/Code -p /home/forever start server.js
+  exec forever --sourceDir=/home/RFID/Code -p /var/run/forever start server.js
   ;;
 
   stop)
   exec forever stop --sourceDir=/home/RFID/Code server.js
+  ;;
+*)
+  echo "Usage: /etc/init.d/start_node {start|stop}"
+  exit 1
   ;;
 esac
 
