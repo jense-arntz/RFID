4ee1618c642d225bb240a517d83de70d10ca7b1a
jense-arntz <jense_arntz@mail.com>
2017-02-02 20:05:05 +0100

    Fixing the code on database design.

--- tagreader/protocol.py
+++ tagreader/protocol.py
@@ -74,6 +74,12 @@ def read_single_tag_id():
     return protocol_packet(TYPE_EPC_COMMAND, 0x00)
 
 
+# EPC Class 1 Generation 2 Command (0x20) /// portal ids (0x1E)
+def portal_ids(timeout, repeat):
+    return protocol_packet(TYPE_EPC_COMMAND, 0x1E, [timeout, repeat])
+
+
+# EPC Class 1 Generation 2 Command (0x20) /// Read Single Tag ID (0x00)
 def sys_antenna_level():
     return protocol_packet(TYPE_SYSTEM_COMMAMD, 0x62)
 
