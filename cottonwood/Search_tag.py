1205adee3c7a50afc928b733851e25908199c406
Jense Arntz <jense_arntz@mail.com>
2017-05-05 23:22:18 +0200

    Add cottonwood search tag module instead tagreader.

---
+++ cottonwood/Search_tag.py
@@ -0,0 +1,257 @@
+#!/usr/bin/python
+
+import logging, serial, time
+from datetime import datetime
+from db import *
+import binascii
+import threading
+
+Cotton_KEY = 1234
+mq = None
+
+logging.basicConfig(filename='/var/log/rfid_card.log', level=logging.INFO)
+
+"""
+Loop Thread Consts
+"""
+# Tag reader Thread.
+tag_reader_thread = None
+tag_reader_thread_event = None
+
+"""
+Command List
+"""
+
+# Firm-/Hardware ID
+FIRMWARE_ID = [0x10, 0x03, 0x00]
+HARDWARE_ID = [0x10, 0x03, 0x01]
+
+# Antenna Power
+ANTENNA_POWER_OFF = [0x18, 0x03, 0x00]
+ANTENNA_POWER_ON = [0x18, 0x03, 0xFF]
+
+# Command Inventory
+START_INVENTORY = [0x31, 0x03, 0x01]
+NEXT_INVENTORY = [0x31, 0x03, 0x02]
+# Write to Tag
+
+"""
+Values
+"""
+ser = None
+BUFF_SIZE = 1024
+BUF_LEN = 12
+
+while ser is None:
+	try:
+		ser = serial.Serial('/dev/ttyUSB0', 9600)
+		logging.info('Cotton Device Found\n')
+	except Exception as e:
+		ser = None
+		print 'Serial Creation Error: {}'.format(e)
+		logging.info('Serial Creation Error: {}\n'.format(e))
+
+
+def read_ser():
+	"""
+	Read serial data from cottonwood.
+	:return:
+	"""
+	tdata = ser.read()           # Wait forever for anything
+	time.sleep(1)              # Sleep (or inWaiting() doesn't give the correct value)
+	data_left = ser.inWaiting()  # Get the number of characters ready to be read
+	tdata += ser.read(data_left)
+	return tdata
+
+
+def Firmware():
+	"""
+	Return Firmware ID
+	:return:
+	"""
+	print 'Reading Firmware ID\n'
+	try:
+		ser.write(bytearray(FIRMWARE_ID))
+		data = read_ser()
+		RES_ID = data[0].encode('hex')
+		RES_len = int(data[1].encode('hex'), 16)
+		firmware_version = data[2:]
+		print 'firmware version: {}\n'.format(firmware_version.encode('hex'))
+		logging.info('firmware version: {}\n'.format(firmware_version.encode('hex')))
+		return True
+
+	except Exception as e:
+		print 'Firmware Error: {}\n'.format(e)
+		return False
+
+
+def Antenna_Power():
+	"""
+	Enable Antenna Power On.
+	:return:
+	"""
+	logging.info('Antenna Power\n')
+	print 'Setting Antenna Power\n'
+	try:
+		ser.write(bytearray(ANTENNA_POWER_ON))
+		data = read_ser()
+		RES_ID = data[0].encode('hex')
+		RES_len = int(data[1].encode('hex'), 16)
+		Rfu = data[2:].encode('hex')
+		print 'Rfu: {}\n'.format(Rfu)
+		logging.info('Rfu: {}'.format(Rfu))
+
+		if Rfu == '00':
+			print 'Success'
+			logging.info("Antenna Power Read Success\n\n")
+			return True
+
+		print 'Antenna Power failed\n\n'
+		logging.info('Antenna Power failed\n\n')
+		return False
+
+	except Exception as e:
+		print 'Antenna Power Error: {}\n'.format(e)
+		logging.info('Antenna Power Error: {}\n'.format(e))
+		return False
+
+
+def Start_Inventory():
+	"""
+	Inventory
+	:return:
+	"""
+	logging.info('Inventory')
+	try:
+		ser.write(bytearray(START_INVENTORY))
+		data = read_ser()
+		logging.info('Inventory: {}'.format(data.encode('hex')))
+		RES_ID = data[0].encode('hex')
+		RES_len = int(data[1].encode('hex'), 16)
+		Found_Tag_Num = int(data[2].encode('hex'), 16)
+		EPC_Len = int(data[3].encode('hex'), 16) - 2  # 2 bytes is reserved bytes.
+		EPC = data[4:5].encode('hex')
+		rfu = data[6:].encode('hex')
+
+		# print 'Inventory: {}'.format(data.encode('hex'))
+		print 'Found_Tag_Num: {}'.format(Found_Tag_Num)
+		print 'EPC: {}\n'.format(EPC)
+		print 'EPC ID: {}\n'.format(rfu)
+
+		return (Found_Tag_Num, rfu)
+	except Exception as e:
+		print 'Inventory Error: {}\n'.format(e)
+		logging.info('Inventory Error: {}\n'.format(e))
+		return False
+
+
+def Next_Inventory():
+	"""
+	Inventory
+	:return:
+	"""
+	logging.info('Inventory')
+	try:
+		ser.write(bytearray(NEXT_INVENTORY))
+		data = read_ser()
+		RES_ID = data[0].encode('hex')
+		RES_len = int(data[1].encode('hex'), 16)
+		Found_Tag_Num = data[2].encode('hex')
+		EPC_Len = int(data[3].encode('hex'), 16) - 2  # 2 bytes is reserved bytes.
+		EPC = data[4:5].encode('hex')
+		rfu = data[6:].encode('hex')
+
+		print 'Inventory: {}'.format(data.encode('hex'))
+		print 'Found_Tag_Num: {}'.format(Found_Tag_Num)
+		print 'EPC: {}\n'.format(EPC)
+		print 'EPC ID: {}\n'.format(rfu)
+
+		return (Found_Tag_Num, rfu)
+	except Exception as e:
+		print 'Inventory Error: {}\n'.format(e)
+		logging.info('Inventory Error: {}\n'.format(e))
+		return False
+
+
+def split_data(num, data):
+	Founded = []
+	for i in range(num):
+		Founded.append(data[i*BUF_LEN: 12])
+
+	return Founded
+
+
+def save_db(card_datas, antenna_number="1"):
+
+	for card_data in card_datas:
+		timestamp = str(datetime.now())
+
+		db1 = reader_db()
+		db1.insert_db(card_data, timestamp, antenna_number)
+
+		print "Inserted EPC: {}, epc_time: {}".format(card_data, timestamp)
+
+
+def main():
+	"""
+	Main Function
+	:return:
+	"""
+
+	while tag_reader_thread_event.isSet():
+		try:
+			Found_EPC = Start_Inventory()
+
+			if Found_EPC:
+				Founded = split_data(Found_EPC[0], Found_EPC[1])
+				logging.info('FOUNDED: {}'.format(Founded))
+				print 'Founded: {}'.format(Founded)
+				threading.Thread(target=save_db, args=(Founded,)).start()
+
+		except Exception as e:
+			logging.info('main error: {}'.format(e))
+			continue
+
+
+# Start thread
+def start_tag_reader_thread(timer):
+	global tag_reader_thread
+	global tag_reader_thread_event
+
+	tag_reader_thread_event = threading.Event()
+	tag_reader_thread_event.set()
+	# tag_reader_thread_event = True
+
+	tag_reader_thread = threading.Thread(target=main)
+	tag_reader_thread.start()
+
+
+# Stop thread
+def stop_tag_reader_thread():
+	global tag_reader_thread
+	global tag_reader_thread_event
+
+	if tag_reader_thread is None or tag_reader_thread_event is None:
+		print "return"
+		return
+	tag_reader_thread_event.clear()
+	print "clear"
+	tag_reader_thread.join()
+	print "join"
+
+	tag_reader_thread = None
+	tag_reader_thread_event = None
+	print "tag_thread ending"
+
+
+if __name__ =='__main__':
+
+	if not Firmware():
+		print 'failed to read Firmware'
+		logging.info('failed to read Firmware')
+
+	if not Antenna_Power():
+		print 'failed to set Antenna Power On'
+		logging.info('failed to set Antenna Power On')
+
+	main()
---
+++ cottonwood/db.py
@@ -0,0 +1,162 @@
+import sqlite3 as sqlite
+import os
+
+
+class reader_db():
+
+    def __init__(self):
+        """
+        Initiate the Database.
+        :return:
+        """
+        self.setting_file = '/home/RFID/reader_setting.db'
+        self.key_table = 'eshow'
+        self.setting_table = 'reader_setting'
+        self.exist_db()
+
+    def exist_db(self):
+        if os.path.isfile('/home/RFID/reader.db'):
+            self.db_file = '/home/RFID/reader.db'
+            self.con = sqlite.connect(self.db_file)
+            self.con.isolation_level = None
+            self.table_name = 'reader'
+            self.cur = self.con.cursor()
+        else:
+            self.create_db()
+            self.db_file = '/home/RFID/reader.db'
+            self.con = sqlite.connect(self.db_file)
+            self.con.isolation_level = None
+            self.table_name = 'reader'
+            self.cur = self.con.cursor()
+
+    def create_db(self):
+        """
+        If no database exists, then create database.
+        :return:
+        """
+        db_file = '/home/RFID/reader.db'
+        table_name_1 = 'reader'
+        fn_1 = 'show_key'
+        ft_1 = 'TEXT'
+        fn_2 = 'client_key'
+        ft_2 = 'TEXT'
+        fn_3 = 'reader_name'
+        ft_3 = 'TEXT'
+        fn_4 = 'mac_address'
+        ft_4 = 'TEXT'
+        fn_5 = 'antenna'
+        ft_5 = 'TEXT'
+        fn_6 = 'card_data'
+        ft_6 = 'TEXT'
+        fn_7 = 'timestamp'
+        ft_7 = 'TEXT'
+        fn_8 = 'custom_field'
+        ft_8 = 'TEXT'
+
+        con = sqlite.connect(db_file)
+        c = con.cursor()
+        c.execute('CREATE TABLE {tn}({nf_1} {ft_1}, {nf_2} {ft_2}, {nf_3} {ft_3}, {nf_4} {ft_4}, {nf_5} {ft_5}'
+                  ', {nf_6} {ft_6}, {nf_7} {ft_7}, {nf_8} {ft_8})'.format(tn=table_name_1,
+                                                           nf_1=fn_1, ft_1=ft_1,
+                                                           nf_2=fn_2, ft_2=ft_2,
+                                                           nf_3=fn_3, ft_3=ft_3,
+                                                           nf_4=fn_4, ft_4=ft_4,
+                                                           nf_5=fn_5, ft_5=ft_5,
+                                                           nf_6=fn_6, ft_6=ft_6,
+                                                           nf_7=fn_7, ft_7=ft_7,
+                                                           nf_8=fn_8, ft_8=ft_8))
+
+        con.commit()
+        con.close()
+
+    def insert_db(self, card_data, timestamp, antenna=None, Custom_data=None):
+        """
+        Insert data into database.
+        :param reg_no:
+        :param pro_time:
+        :return:
+        """
+        try:
+            self.exist_db()
+            data = self.get_setting()
+
+            sql = 'INSERT INTO {table_name}(show_key, client_key, reader_name, mac_address, card_data, ' \
+                  'timestamp, antenna, custom_field) VALUES(?,?,?,?,?,?,?,?)'.format(table_name=self.table_name)
+            self.cur.execute(sql, (data['show_key'], data['client_key'], data['reader_name'], data['mac_address'],
+                                   card_data, timestamp, antenna, Custom_data))
+
+            self.con.commit()
+
+        except Exception as e:
+            print 'db exception : {}'.format(e)
+            self.con.rollback()
+
+    def del_db(self):
+        """
+        Delete data from database.
+        :param EPC:
+        :return:
+        """
+
+        self.exist_db()
+        try:
+            sql = 'DELETE FROM {table_name}'.format(table_name=self.table_name)
+            print 'reg_no None'
+            self.cur.execute(sql)
+            self.cur.execute('VACUUM')
+            self.con.commit()
+        except Exception as e:
+            print 'deleting db Exception: {}'.format(e)
+            self.con.rollback()
+
+    def update_db(self, card_data, timestamp, Name=None):
+        """
+        Update data in database.
+        :param reg_no:
+        :param pro_time:
+        :return:
+        """
+        self.exist_db()
+        sql = 'UPDATE {table_name} SET timestamp=? WHERE card_data={card_data}'.format(table_name=self.table_name, card_data=card_data)
+
+        self.cur.execute(sql, (timestamp,))
+
+        self.con.commit()
+
+    def sel_db(self):
+        """
+        Get data from database.
+        :return:
+        """
+        global LENGTH
+        # global FLAG
+        self.exist_db()
+        sql = 'SELECT * FROM {table_name}'.format(table_name=self.table_name)
+        result = self.cur.execute(sql)
+        data = result.fetchall()
+        return data
+
+    def get_setting(self):
+        try:
+            self.con_setting = sqlite.connect(self.setting_file)
+            self.cur_setting = self.con_setting.cursor()
+            sql_setting = 'SELECT * FROM {table_name}'.format(table_name=self.setting_table)
+            sql_show_key = 'SELECT * FROM {table_name}'.format(table_name=self.key_table)
+            setting_data = self.cur_setting.execute(sql_setting).fetchone()
+            show_key_data = self.cur_setting.execute(sql_show_key).fetchone()
+
+            return {
+                    'reader_name': setting_data[1],
+                    'mac_address': setting_data[2],
+                    'show_key': show_key_data[1],
+                    'client_key': show_key_data[2]
+                   }
+        except Exception as e:
+            return {
+                    'reader_name': 'None',
+                    'mac_address': 'None',
+                    'show_key': 'None',
+                    'client_key': 'None'
+                   }
+
+db = reader_db()
